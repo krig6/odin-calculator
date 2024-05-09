38b1aebeeee58a0ff1a13c6c03e4ef15e6147c96
krig6 <ej.ldsm@gmail.com>
2024-05-09 19:47:59 +0800

    Create variable for our DOM element selectors

---
+++ script.js
@@ -0,0 +1,9 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButton = document.querySelectorAll('[data-operator]');
+const clearButton = document.querySelector('[data-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const equalButton = document.querySelector('[data-equal]');
+const previousInputText = document.querySelector('[data-previous-input]');
+const currentInputText = document.querySelector('[data-current-input]');
+
+
