5449805dcd2e84f08cc69e8bc5680228806201a6
krig6 <ej.ldsm@gmail.com>
2024-05-09 20:24:59 +0800

    Create event listener for operator buttons
    Implement selectOperator method so we can display operator selected.
    
    Also,display number and operator in previous input for interactive experience.

--- script.js
+++ script.js
@@ -15,12 +15,23 @@ class Calculator {
     }
     // Append numbers for displaying
     appendNumber(number) {
+        // Avoid decimal point to be entered twice 
         if (number === '.' && this.currentInput.includes('.')) return;
+
+        // Instead of 1+1 = 2, it will become 1+1 = 11
         this.currentInput = this.currentInput.toString() + number.toString();
     }
     // Select the operator 
-    selectOperator() {
-
+    selectOperator(operator) {
+        // if number is not present, clicking an operator will do nothing
+        if (this.currentInput === '') return
+        // If previous number has a value already, we can proceed to calculate without clicking equal sign
+        if (this.previousInput !== '') {
+            this.calculate();
+        }
+        this.operator = operator;
+        this.previousInput = this.currentInput;
+        this.currentInput = '';
     }
     // Calculate 
     calculate() {
@@ -38,7 +49,12 @@ class Calculator {
     }
     // Update display 
     updateDisplay() {
-        this.currentInputText.textContent = this.currentInput
+        this.currentInputText.textContent = this.currentInput;
+        if (this.operator != null) {
+            this.previousInputText.textContent = this.previousInput + this.operator;
+        } else {
+            this.previousInputText.textContent = '';
+        }
     }
 }
 
@@ -48,6 +64,13 @@ const calculator = new Calculator(previousInputText, currentInputText);
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.textContent);
+        calculator.updateDisplay();
+    })
+})
+
+operatorButton.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.selectOperator(button.textContent);
         calculator.updateDisplay();
     })
 })
