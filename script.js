198cb60a75d54aab8f4b275bdcd32900dd20a341
krig6 <ej.ldsm@gmail.com>
2024-05-09 19:57:11 +0800

    Create class Calculator

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ const previousInputText = document.querySelector('[data-previous-input]');
 const currentInputText = document.querySelector('[data-current-input]');
 
 
+class Calculator {
+    constructor(previousInputText, currentInputText) {
+        this.previousInputText = previousInputText;
+        this.currentInputText = currentInputText;
+    }
+    // append numbers to display
+    appendToDisplay() {
+
+    }
+    // select the operator 
+    selectOperator() {
+
+    }
+    // calculate 
+    calculate() {
+
+    }
+    // clear display
+    clear() {
+
+    }
+    // delete numbers in display
+    delete() {
+
+    }
+    //update display 
+    updateDisplay() {
+
+    }
+}
